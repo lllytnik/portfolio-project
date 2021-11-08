d06e744f6e00037a77f9275af9a734fd03dc0a31
lllytnik <65924940+lllytnik@users.noreply.github.com>
2021-11-08 12:04:47 +0400

    work on design

--- src/assets/js/marquee.js
+++ src/assets/js/marquee.js
@@ -1,20 +1,3 @@
-$('img.img-svg').each(function () {
-    var $img = $(this);
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
-    $.get(imgURL, function (data) {
-        var $svg = $(data).find('svg');
-        if (typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', imgClass + ' replaced-svg');
-        }
-        $svg = $svg.removeAttr('xmlns:a');
-        if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-        }
-        $img.replaceWith($svg);
-    }, 'xml');
-});
-
 function animateMarquee(el, duration) {
     const innerEl = el.querySelector('.caroussel__inner');
     const innerWidth = innerEl.offsetWidth;
@@ -44,5 +27,3 @@ function animateMarquee(el, duration) {
 const marquee = document.querySelector('#marquee');
 
 animateMarquee(marquee, 30000);
-
-//# sourceMappingURL=script.js.map
