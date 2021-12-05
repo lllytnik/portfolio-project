800f8cccd3732784639fd27ffbf6323535969022
lllytnik <65924940+lllytnik@users.noreply.github.com>
2021-12-05 23:24:16 +0400

    revision  menu burger

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function () {
     $('.header__burger').click(function (event) {
-        $('.header__burger,.header__nav').toggleClass('active');
+        $('.header__burger, .header__nav, .header__top, .social__list, .header__social').toggleClass('active');
         $('body').toggleClass('lock');
     });
     $('.header__nav').click(function () {
-        $('.header__burger, .header__nav').removeClass('active');
+        $('.header__burger, .header__nav, .header__top, .social__list, .header__social').removeClass('active');
         $('body').removeClass('lock');
     });
 });
